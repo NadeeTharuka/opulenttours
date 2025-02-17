01c3c21321e2bb8d3d232c7e2ee236d95d6a095f
Nadee Tharuka <121155318+NadeeTharuka@users.noreply.github.com>
2025-02-17 22:03:19 +0530

    initial commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,238 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+        <div className="container-fluid bg-primary py-5 mb-5 hero-header">
+    <div className="container py-5">
+      <div className="row justify-content-center py-5">
+        <div className="col-lg-10 pt-lg-5 mt-lg-5 text-center">
+          <h1 className="display-3 text-white animated slideInDown">
+            About Us
+          </h1>
+          <nav aria-label="breadcrumb">
+            <ol className="breadcrumb justify-content-center">
+              <li className="breadcrumb-item">
+                <a href="/">Home</a>
+              </li>
+              {/* <li className="breadcrumb-item">
+                <a href="/Pages">Pages</a>
+              </li> */}
+              <li
+                className="breadcrumb-item text-white active"
+                aria-current="page"
+              >
+                About
+              </li>
+            </ol>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </div>
+        
+            {/* About Start */}
+    <div className="container-xxl py-5">
+      <div className="container">
+        <div className="row g-5">
+          <div
+            className="col-lg-6 wow fadeInUp"
+            data-wow-delay="0.1s"
+            style={{ minHeight: 400 }}
+          >
+            <div className="position-relative h-100">
+              <img
+                className="img-fluid position-absolute w-100 h-100"
+                src="assets/img/about.jpg"
+                alt=""
+                style={{ objectFit: "cover" }}
+              />
+            </div>
+          </div>
+          <div className="col-lg-6 wow fadeInUp" data-wow-delay="0.3s">
+            <h6 className="section-title bg-white text-start text-primary pe-3">
+              About Us
+            </h6>
+            <h1 className="mb-4">
+              Welcome to <span className="text-primary">Opulent Tours</span>
+            </h1>
+            <p className="mb-4">
+            🌏 “Opulent” based in Kalutara, Sri Lanka, brings 15 years of travel expertise to life with a young and creative team. Specializing in unforgettable trips across Sri Lanka, India, and Thailand, Wego offers a wide range of travel experiences, from exotic getaways to thrilling adventures.
+</p>
+            <p className="mb-4">
+🛫 Discover your next adventure with us! Opulent, a renowned travel agency based in Kalutara, Sri Lanka, combines 15 years of industry expertise with the fresh energy of a young, innovative team to create exceptional travel experiences. Our passion is to transform your travel dreams into reality by offering a wide range of tailored journeys. From the pristine beaches of Sri Lanka to the vibrant cultures of India and the tropical beauty of Thailand, we specialize in curating unforgettable trips that cater to every kind of traveler. Whether you're seeking a luxurious escape to a tranquil island, an exhilarating adventure through dense rainforests, or an immersive cultural tour through ancient cities, Wego has something for everyone. Our diverse offerings range from exotic getaways in paradise locations to adrenaline-pumping experiences, all meticulously planned to ensure a seamless and stress-free journey.
+</p>
+
+<p className="mb-4">
+⛺ At Opulent, we believe that travel is more than just visiting a destination—it's about creating memories that last a lifetime. Our dedicated team is committed to providing personalized services, from custom itineraries to insider recommendations, ensuring that every trip you take with us is truly one-of-a-kind.
+</p>
+
+            
+            <p className="mb-4">
+            🚁 Let us help you discover new horizons, explore hidden gems, and embark on adventures that will leave you enriched and inspired. Your next extraordinary journey awaits with Opulent!
+            </p>
+            <div className="row gy-2 gx-4 mb-4">
+              <div className="col-sm-6">
+                <p className="mb-0">
+                  <i className="fa fa-arrow-right text-primary me-2" />
+                  First Class Flights
+                </p>
+              </div>
+              <div className="col-sm-6">
+                <p className="mb-0">
+                  <i className="fa fa-arrow-right text-primary me-2" />
+                  Handpicked Hotels
+                </p>
+              </div>
+              <div className="col-sm-6">
+                <p className="mb-0">
+                  <i className="fa fa-arrow-right text-primary me-2" />5 Star
+                  Accommodations
+                </p>
+              </div>
+              <div className="col-sm-6">
+                <p className="mb-0">
+                  <i className="fa fa-arrow-right text-primary me-2" />
+                  Latest Model Vehicles
+                </p>
+              </div>
+              <div className="col-sm-6">
+                <p className="mb-0">
+                  <i className="fa fa-arrow-right text-primary me-2" />
+                  150 Premium City Tours
+                </p>
+              </div>
+              <div className="col-sm-6">
+                <p className="mb-0">
+                  <i className="fa fa-arrow-right text-primary me-2" />
+                  24/7 Service
+                </p>
+              </div>
+            </div>
+            <a className="btn btn-primary py-3 px-5 mt-2" href="">
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    {/* About End */}
+    {/* Team Start */}
+    <div className="container-xxl py-5">
+      <div className="container">
+        <div className="text-center wow fadeInUp" data-wow-delay="0.1s">
+          <h6 className="section-title bg-white text-center text-primary px-3">
+            Travel Guide
+          </h6>
+          <h1 className="mb-5">Meet Our Guide</h1>
+        </div>
+        <div className="row g-4">
+          <div className="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+            <div className="team-item">
+              <div className="overflow-hidden">
+                <img className="img-fluid" src="assets/img/team-1.jpg" alt="" />
+              </div>
+              <div
+                className="position-relative d-flex justify-content-center"
+                style={{ marginTop: "-19px" }}
+              >
+                <a className="btn btn-square mx-1" href="">
+                  <i className="fab fa-facebook-f" />
+                </a>
+                <a className="btn btn-square mx-1" href="">
+                  <i className="fab fa-twitter" />
+                </a>
+                <a className="btn btn-square mx-1" href="">
+                  <i className="fab fa-instagram" />
+                </a>
+              </div>
+              <div className="text-center p-4">
+                <h5 className="mb-0">Full Name</h5>
+                <small>Designation</small>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
+            <div className="team-item">
+              <div className="overflow-hidden">
+                <img className="img-fluid" src="assets/img/team-2.jpg" alt="" />
+              </div>
+              <div
+                className="position-relative d-flex justify-content-center"
+                style={{ marginTop: "-19px" }}
+              >
+                <a className="btn btn-square mx-1" href="">
+                  <i className="fab fa-facebook-f" />
+                </a>
+                <a className="btn btn-square mx-1" href="">
+                  <i className="fab fa-twitter" />
+                </a>
+                <a className="btn btn-square mx-1" href="">
+                  <i className="fab fa-instagram" />
+                </a>
+              </div>
+              <div className="text-center p-4">
+                <h5 className="mb-0">Full Name</h5>
+                <small>Designation</small>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
+            <div className="team-item">
+              <div className="overflow-hidden">
+                <img className="img-fluid" src="assets/img/team-3.jpg" alt="" />
+              </div>
+              <div
+                className="position-relative d-flex justify-content-center"
+                style={{ marginTop: "-19px" }}
+              >
+                <a className="btn btn-square mx-1" href="">
+                  <i className="fab fa-facebook-f" />
+                </a>
+                <a className="btn btn-square mx-1" href="">
+                  <i className="fab fa-twitter" />
+                </a>
+                <a className="btn btn-square mx-1" href="">
+                  <i className="fab fa-instagram" />
+                </a>
+              </div>
+              <div className="text-center p-4">
+                <h5 className="mb-0">Full Name</h5>
+                <small>Designation</small>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.7s">
+            <div className="team-item">
+              <div className="overflow-hidden">
+                <img className="img-fluid" src="assets/img/team-4.jpg" alt="" />
+              </div>
+              <div
+                className="position-relative d-flex justify-content-center"
+                style={{ marginTop: "-19px" }}
+              >
+                <a className="btn btn-square mx-1" href="">
+                  <i className="fab fa-facebook-f" />
+                </a>
+                <a className="btn btn-square mx-1" href="">
+                  <i className="fab fa-twitter" />
+                </a>
+                <a className="btn btn-square mx-1" href="">
+                  <i className="fab fa-instagram" />
+                </a>
+              </div>
+              <div className="text-center p-4">
+                <h5 className="mb-0">Full Name</h5>
+                <small>Designation</small>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    {/* Team End */}
+    </div>
+  )
+}
+
+export default About
